7a5281c2325af0b8320b6ef33ecfde3eaafa110d
vladyslavkosenko <vaduha91@gmail.com>
2023-01-07 19:25:15 +0200

    set up the database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-// const models = require('./models/models')
-// const cors = require('cors')
-// const fileUpload = require('express-fileupload')
-// const router = require('./routes/index')
-// const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-// const path = require('path')
 
 
 const PORT = process.env.PORT || 5000
